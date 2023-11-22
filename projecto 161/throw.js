219096652be8a078808eda90d7f646e6e2df903f
TwilightNightWing <88166572+TwilightNightWing@users.noreply.github.com>
2023-11-22 16:50:11 -0600

    Add files via upload

---
+++ projecto 161/throw.js
@@ -0,0 +1,47 @@
+AFRAME.registerComponent("bowling-balls", {
+    init: function () {
+      this.shootBullet();
+    },
+    shootBullet: function () {
+      window.addEventListener("keydown", (e) => {
+        if (e.key === "z") {
+          var bullet = document.createElement("a-entity");
+  
+          bullet.setAttribute("geometry", {
+            primitive: "sphere",
+            radius: 0.1,
+          });
+  
+          bullet.setAttribute("material", "color", "black");
+  
+          var cam = document.querySelector("#camera");
+  
+          pos = cam.getAttribute("position");
+  
+          bullet.setAttribute("position", {
+            x: pos.x,
+            y: pos.y,
+            z: pos.z,
+          });
+  
+          var camera = document.querySelector("#camera").object3D;
+  
+          // Obtener la dirección de la cámara como un vector de Three.js
+          var direction = new THREE.Vector3();
+          camera.getWorldDirection(direction);
+  
+          // Establecer la velocidad y su dirección
+          bullet.setAttribute("velocity", direction.multiplyScalar(-10));
+          bullet.setAttribute("dynamic-body", {
+            shape: "sphere",
+            mass: "0",
+           
+          })
+  
+          var scene = document.querySelector("#scene");
+  
+          scene.appendChild(bullet);
+        }
+      });
+    },
+})
